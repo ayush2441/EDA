39956aa1673c5f28e920e00e480c9f8434c55794
ayush2441 <90455952+ayush2441@users.noreply.github.com>
2024-09-18 06:16:59 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,3 +1,16 @@
-import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/CourseMaterial/DataWrangling/main/flowerdataset.csv')
-print(df.head())
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+
+# Download necessary NLTK data if not already downloaded
+nltk.download('punkt')
+
+# Sample sentence
+sentence = "This is a sample sentence for tokenization."
+
+# Tokenize sentence into words
+words = word_tokenize(sentence)
+print("Words:", words)
+
+# Tokenize sentence into sentences
+sentences = sent_tokenize(sentence)
+print("Sentences:", sentences)
